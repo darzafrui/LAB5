6ce532c8e984268831127536f3bf43dde49d1f0a
darzafrui <darzafrui@alum.us.es>
2024-04-08 18:33:00 +0200

    hasta_parte2_lab0

--- DeliverUS-Frontend-Owner/App.js
+++ DeliverUS-Frontend-Owner/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
-import Layout from './src/screens/Layout'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import * as React from 'react'
+import ControlPanelScreen from './src/screens/controlPanel/ControlPanelScreen'
+import ProfileScreen from './src/screens/profile/ProfileScreen'
+import RestaurantsScreen from './src/screens/restaurants/RestaurantsScreen'
+
+const Tab = createBottomTabNavigator()
 
 export default function App () {
   return (
-    <Layout />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Restaurants"
+          component={RestaurantsScreen} />
+        <Tab.Screen
+          name="Control Panel"
+          component={ControlPanelScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
